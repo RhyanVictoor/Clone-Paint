bba51c536b7d329bde9bb4d972995204026508f2
RhyanVictor <rhyansinho.04@gmail.com>
2024-09-07 20:26:06 -0400

    java Script

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,5 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
-
 const inputColor = document.querySelector(".input__color");
 const tools = document.querySelectorAll(".button__tool");
 const sizeButtons = document.querySelectorAll(".button__size");
